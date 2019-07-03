5493799f529c0fa16c4b270ad80c22a38036e414
onewoorks <irwanbibrahim@onewoorks.com>
2019-07-04 07:17:41 +0800

    day 2

--- 03-Fade-LED/03-Fade-LED.ino
+++ 03-Fade-LED/03-Fade-LED.ino
@@ -1,34 +1,18 @@
+int brightness = 60;
+
 void setup() {
   // put your setup code here, to run once:
   pinMode(5, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  analogWrite(5,60);
+  analogWrite(5, brightness);
   delay(200);
-  analogWrite(5,55);
-  delay(200);
-  analogWrite(5,50);
-  delay(200);
-  analogWrite(5,45);
-  delay(200);
-  analogWrite(5,40);
-  delay(200);
-  analogWrite(5,35);
-  delay(200);
-  analogWrite(5,30);
-  delay(200);
-  analogWrite(5,25);
-  delay(200);
-  analogWrite(5,20);
-  delay(200);
-  analogWrite(5,15);
-  delay(200);
-  analogWrite(5,10);
-  delay(200);
-  analogWrite(5,5);
-  delay(200);
-  analogWrite(5,0);
-  delay(1000);
+
+  if(brightness == 0){
+    delay(1000);
+    brightness = 60;
+  } else {
+    brightness -= 5;
+  }
 }
---
+++ 04-Tone/04-Tone.ino
@@ -0,0 +1,20 @@
+void setup() {
+  pinMode(8, OUTPUT);
+}
+
+void loop() {
+  tone(8, 262, 250);
+  delay(325);
+  tone(8, 294, 250);
+  delay(325);
+  tone(8, 330, 250);
+  delay(325);
+  tone(8, 349, 250);
+  delay(325);
+  tone(8, 392, 250);
+  delay(325);
+  tone(8, 440, 250);
+  delay(325);
+  tone(8, 494, 250);
+  delay(1000);  
+}
